c784bafbeeb98318bfc28a5ab5c7534652927239
tolubishops0 <toluokunjoyo@gmail.com>
2022-12-12 12:50:46 +0100

    setup react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+} from "react-router-dom";
+
+import Layout from "./Layouts/DefaultLayout";
 
 function App() {
-
   return (
     <div className="App">
-      <h1 className="text-red-500">hello</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route index element={<Home />} />
+          <Route path="about" element={<About />} /> */}
+          {/* <Route path="dashboard" element={<Dashboard />} /> */}
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          {/* <Route path="*" element={<NoMatch />} /> */}
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
