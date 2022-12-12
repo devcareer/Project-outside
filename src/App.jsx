9841b2eafaa31a54035b3ab223b43d67df063aee
Treasure Ajefu <treasureajefu@gmail.com>
2022-12-12 11:53:54 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-red-500">hello world</h1>
+    </div>
+  )
+}
+
+export default App
