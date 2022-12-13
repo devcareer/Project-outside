6520655a5353dd8b00f5841fd1f19e627a555f07
Daniel Ipogah <darnielipogah@gmail.com>
2022-12-13 12:48:34 +0100

    Add: venue page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Venue from "./pages/Venue";
 
 function App() {
-
   return (
     <div className="App">
-      <h1 className="text-red-500">hello</h1>
+      {/* <h1 className="text-red-500">hello</h1> */}
+      <Venue />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Venue.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+
+export default function Venue() {
+  return (
+    <div>
+      <div className="flex justify-center items-center flex-col mt-2">
+        <div className="   hover:bg-gray-200 transition ease-out duration-500 hover:scale-105 hover:opacity-70 mb-6 flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
+          <div className=" h-[80px] md:h-[100px] w-[902px] ml-[5px] flex  items-center ">
+            <div>
+              <img
+                src="src\assets\Rectangle 2.png"
+                className=" ml-[25px] w-[45px] h-[45px] md:w-[60px] md:h-[60px] md:ml-[40px]"
+              />
+            </div>
+            <div className=" ml-3 md:ml-[30px] ">
+              <h1 className=" text-[13px]  leading-5 font-semibold md:leading-8 md:text-base">
+                Park Square Live Music & Coffee
+              </h1>
+              <p className="font-semibold text-[10px] leading-3 md:text-sm text-[#626F86] md:leading-6">
+                No.3,Obansanjo rd,Yaba,Lagos State
+              </p>
+            </div>
+            <div>
+              <img
+                src="src\assets\🧬 chevron-right-circle - Light.svg"
+                className="w-[22px] h-[22px] ml-[25px] md:w-[25px] md:h-[25px]  md:ml-[445px]  "
+              />
+            </div>
+          </div>
+        </div>
+        <div className="  hover:bg-gray-200 transition ease-out duration-500 hover:scale-105 hover:opacity-70  mb-6 flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
+          <div className=" h-[80px] md:h-[100px] w-[902px] ml-[5px] flex  items-center ">
+            <div>
+              <img
+                src="src\assets\Rectangle 2 (1).png"
+                className=" ml-[25px] w-[45px] h-[45px] md:w-[60px] md:h-[60px] md:ml-[40px]"
+              />
+            </div>
+            <div className=" ml-3 md:ml-[30px] ">
+              <h1 className=" text-[13px]  leading-5 font-semibold md:leading-8 md:text-base">
+                Park Square Live Music & Coffee
+              </h1>
+              <p className="font-semibold text-[10px] leading-3 md:text-sm text-[#626F86] md:leading-6">
+                No.3,Obansanjo rd,Yaba,Lagos State
+              </p>
+            </div>
+            <div>
+              <img
+                src="src\assets\🧬 chevron-right-circle - Light.svg"
+                className="w-[22px] h-[22px] ml-[25px] md:w-[25px] md:h-[25px]  md:ml-[445px]  "
+              />
+            </div>
+          </div>
+        </div>
+        <div className="   hover:bg-gray-200 transition ease-out duration-500 hover:scale-105 hover:opacity-70 mb-6 flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
+          <div className=" h-[80px] md:h-[100px] w-[902px] ml-[5px] flex  items-center ">
+            <div>
+              <img
+                src="src\assets\Rectangle 2 (3).png"
+                className=" ml-[25px] w-[45px] h-[45px] md:w-[60px] md:h-[60px] md:ml-[40px]"
+              />
+            </div>
+            <div className=" ml-3 md:ml-[30px] ">
+              <h1 className=" text-[13px]  leading-5 font-semibold md:leading-8 md:text-base">
+                Park Square Live Music & Coffee
+              </h1>
+              <p className="font-semibold text-[10px] leading-3 md:text-sm text-[#626F86] md:leading-6">
+                No.3,Obansanjo rd,Yaba,Lagos State
+              </p>
+            </div>
+            <div>
+              <img
+                src="src\assets\🧬 chevron-right-circle - Light.svg"
+                className="w-[22px] h-[22px] ml-[25px] md:w-[25px] md:h-[25px]  md:ml-[445px]  "
+              />
+            </div>
+          </div>
+        </div>
+        <div className="   hover:bg-gray-200 transition ease-out duration-500 hover:scale-105 hover:opacity-70 mb-6 flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
+          <div className=" h-[80px] md:h-[100px] w-[902px] ml-[5px] flex  items-center ">
+            <div>
+              <img
+                src="src\assets\Rectangle 2 (2).png"
+                className=" ml-[25px] w-[45px] h-[45px] md:w-[60px] md:h-[60px] md:ml-[40px]"
+              />
+            </div>
+            <div className=" ml-3 md:ml-[30px] ">
+              <h1 className=" text-[13px]  leading-5 font-semibold md:leading-8 md:text-base">
+                Park Square Live Music & Coffee
+              </h1>
+              <p className="font-semibold text-[10px] leading-3 md:text-sm text-[#626F86] md:leading-6">
+                No.3,Obansanjo rd,Yaba,Lagos State
+              </p>
+            </div>
+            <div>
+              <img
+                src="src\assets\🧬 chevron-right-circle - Light.svg"
+                className="w-[22px] h-[22px] ml-[25px] md:w-[25px] md:h-[25px]  md:ml-[445px]  "
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-around">
+        <div className=" text-[10px] md:text-[14px] leading-5 font-medium">
+          Showing 10 of 30
+        </div>
+        <div className="flex  ">
+          <div className=" mr-6 text-[13px] h-[30px] w-[70px]  md:h-[41px] md:w-[132px]  hover:bg-gray-600  transition ease-out duration-500 hover:scale-105 hover:opacity-50 text-[#FFFFFF] border rounded-[20px] flex items-center justify-center bg-[#0C66E4] md:text-[16px] md:leading-8 md:font-semibold">
+            Prev
+          </div>
+          <div className=" text-[13px] h-[30px] w-[70px] hover:bg-gray-300  transition ease-out duration-500 hover:scale-105 hover:opacity-80 md:h-[41px] md:w-[132px] text-[#0C66E4] border rounded-[20px] flex items-center justify-center bg-[#E9F2FF]  md:text-[16px] md:leading-8 md:font-semibold">
+            Next
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
