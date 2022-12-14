21b1089344baf7257dee31a4383659dae2528166
Treasure Ajefu <treasureajefu@gmail.com>
2022-12-14 11:30:01 +0100

    minor changes to sizing and spacing

--- src/pages/Shows.jsx
+++ src/pages/Shows.jsx
@@ -67,13 +67,13 @@ function Shows() {
     ]
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-12 lg:gap:16 w-10/12 mx-auto my-24 max-w-4xl xl:max-w-5xl">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-10 md:gap-14 lg:gap-18 w-10/12 mx-auto my-24 max-w-4xl xl:max-w-5xl">
             {shows.map((show) => (
-                <div key={show.id} className="bg-[#F7F8F9] flex flex-col items-center justify-center rounded-2xl p-7">
-                    <img src={sammy} alt="" />
+                <div key={show.id} className="bg-[#F7F8F9] flex flex-col items-center justify-center rounded-[60px] p-7">
+                    <img className="rounded-[20px] w-36 h-36 object-cover" src={sammy} alt="" />
                     <p className="text-[#626F86] my-2">{show.date}</p>
                     <Link to="/shows" className="text-xl font-medium underline">{show.performer}</Link>
-                    <span className="my-2">performing at</span>
+                    <span className="my-2 text-[#626F86]">performing at</span>
                     <Link to="/shows" className="text-xl text-center font-medium underline">{show.venue}</Link>
                 </div>
             ))}
