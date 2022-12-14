3b20673accb1de37ce74ce0e59d1295d515221fb
Daniel Ipogah <darnielipogah@gmail.com>
2022-12-14 10:19:04 +0100

    feat: put pagination in a seperate route

--- src/pages/Venue.jsx
+++ src/pages/Venue.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Pagination from "../components/pagination";
 
 export default function Venue() {
   return (
     <div>
       <div className="flex justify-center items-center flex-col mt-2">
-        <div className="   hover:bg-gray-200 transition ease-out duration-500 hover:scale-105 hover:opacity-70 mb-6 flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
+        <div className="   hover:bg-gray-200 transition cursor-pointer ease-out duration-500 hover:scale-105 hover:opacity-70 mb-6 flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
           <div className=" h-[80px] md:h-[100px] w-[902px] ml-[5px] flex  items-center ">
             <div>
               <img
@@ -28,7 +29,7 @@ export default function Venue() {
             </div>
           </div>
         </div>
-        <div className="  hover:bg-gray-200 transition ease-out duration-500 hover:scale-105 hover:opacity-70  mb-6 flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
+        <div className="  hover:bg-gray-200 transition cursor-pointer ease-out duration-500 hover:scale-105 hover:opacity-70  mb-6 flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
           <div className=" h-[80px] md:h-[100px] w-[902px] ml-[5px] flex  items-center ">
             <div>
               <img
@@ -52,7 +53,7 @@ export default function Venue() {
             </div>
           </div>
         </div>
-        <div className="   hover:bg-gray-200 transition ease-out duration-500 hover:scale-105 hover:opacity-70 mb-6 flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
+        <div className="   hover:bg-gray-200 transition cursor-pointer ease-out duration-500 hover:scale-105 hover:opacity-70 mb-6 flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
           <div className=" h-[80px] md:h-[100px] w-[902px] ml-[5px] flex  items-center ">
             <div>
               <img
@@ -76,7 +77,7 @@ export default function Venue() {
             </div>
           </div>
         </div>
-        <div className="   hover:bg-gray-200 transition ease-out duration-500 hover:scale-105 hover:opacity-70  flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
+        <div className="   hover:bg-gray-200 transition cursor-pointer ease-out duration-500 hover:scale-105 hover:opacity-70  flex w-[350px] h-[80px] bg-[#F7F8F9]  border rounded-[60px] md:h-[100px] md:w-[65%]">
           <div className=" h-[80px] md:h-[100px] w-[902px] ml-[5px] flex  items-center ">
             <div>
               <img
@@ -101,19 +102,7 @@ export default function Venue() {
           </div>
         </div>
       </div>
-      <div className="flex justify-around mt-[62px] ">
-        <div className=" text-[10px] md:text-[14px] leading-5 font-medium">
-          Showing 10 of 30
-        </div>
-        <div className="flex  ">
-          <div className=" mr-6 text-[13px] h-[30px] w-[70px]  md:h-[41px] md:w-[132px]  hover:bg-gray-600  transition ease-out duration-500 hover:scale-105 hover:opacity-50 text-[#FFFFFF] border rounded-[20px] flex items-center justify-center bg-[#0C66E4] md:text-[16px] md:leading-8 md:font-semibold">
-            Prev
-          </div>
-          <div className=" text-[13px] h-[30px] w-[70px] hover:bg-gray-300  transition ease-out duration-500 hover:scale-105 hover:opacity-80 md:h-[41px] md:w-[132px] text-[#0C66E4] border rounded-[20px] flex items-center justify-center bg-[#E9F2FF]  md:text-[16px] md:leading-8 md:font-semibold">
-            Next
-          </div>
-        </div>
-      </div>
+      <Pagination />
     </div>
   );
 }
