487176db3da8cb740e7015e24ea9e584c02edafe
Ojonugwa Alikali <alikali.ojonugwajustice@gmail.com>
2022-12-15 12:40:36 +0100

    Set up eslint, prettier, and pre-commit hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Routes,
-  Route,
-  Outlet,
-  Link,
-} from "react-router-dom";
-import Shows from "./pages/Shows";
-import Layout from "./Layouts/DefaultLayout";
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Shows from './pages/Shows';
+import Layout from './Layouts/DefaultLayout';
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-        <Route path="/shows" element={<Shows />} />
-          {/* <Route index element={<Home />} />
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route path="/shows" element={<Shows />} />
+					{/* <Route index element={<Home />} />
           <Route path="about" element={<About />} /> */}
-          {/* <Route path="dashboard" element={<Dashboard />} /> */}
+					{/* <Route path="dashboard" element={<Dashboard />} /> */}
 
-          {/* Using path="*"" means "match anything", so this route
+					{/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-          {/* <Route path="*" element={<NoMatch />} /> */}
-        </Route>
-      </Routes>
-    </div>
-  );
+					{/* <Route path="*" element={<NoMatch />} /> */}
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
